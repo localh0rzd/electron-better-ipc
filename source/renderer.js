82d4403f50c2ba51057c324a45d899f676a77e08
Andrej Beliakov <beliakov@optimal-systems.de>
2018-08-24 10:10:10 +0200

    add ipcRenderer.sendToHost() wrapper

--- source/renderer.js
+++ source/renderer.js
@@ -25,6 +25,43 @@ ipc.callMain = (channel, data) => new Promise((resolve, reject) => {
 	ipc.send(sendChannel, data);
 });
 
+ipc.callHost = (channel, data) => new Promise((resolve, reject) => {
+	const {sendChannel, dataChannel, errorChannel} = util.getResponseChannels(channel);
+
+	const cleanup = () => {
+		ipc.removeAllListeners(dataChannel);
+		ipc.removeAllListeners(errorChannel);
+	};
+
+	ipc.on(dataChannel, (event, result) => {
+		cleanup();
+		resolve(result);
+	});
+
+	ipc.on(errorChannel, (event, error) => {
+		cleanup();
+		reject(error);
+	});
+
+	ipc.sendToHost(sendChannel, data);
+});
+
+ipc.answerHostedRenderer = (channel, callback) => {
+	const {sendChannel, dataChannel, errorChannel} = util.getResponseChannels(channel);
+
+	ipc.on(sendChannel, async (event, data) => {
+		const send = (channel, data) => {
+			event.sender.send(channel, data);
+		};
+
+		try {
+			send(dataChannel, await callback(data));
+		} catch (error) {
+			send(errorChannel, error);
+		}
+	});
+};
+
 ipc.answerMain = (channel, callback) => {
 	const window = electron.remote.getCurrentWindow();
 	const {sendChannel, dataChannel, errorChannel} = util.getRendererResponseChannels(window.id, channel);
